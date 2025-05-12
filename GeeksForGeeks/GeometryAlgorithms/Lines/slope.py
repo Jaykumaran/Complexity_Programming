d7ba007ef2fa10a3ef1dee374a842a530dd3e5a9
Jaykumaran R <60032500+Jaykumaran@users.noreply.github.com>
2025-05-12 18:09:26 +0530

    Create slope.py

---
+++ GeeksForGeeks/GeometryAlgorithms/Lines/slope.py
@@ -0,0 +1,10 @@
+def slope(x1, y1, x2, y2):
+    if x1 == x2: # They meet vertically
+       return float('inf')
+    m = (y2-y1) / (x2 - x1)
+    return m
+
+# https://www.geeksforgeeks.org/program-find-slope-line/
+    
+
+  
