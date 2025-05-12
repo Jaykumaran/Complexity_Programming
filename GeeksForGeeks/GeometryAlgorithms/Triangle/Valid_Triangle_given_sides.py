ad544f8dc67ed740c4f16d6b53d28f4c7c261ee5
Jaykumaran R <60032500+Jaykumaran@users.noreply.github.com>
2025-05-12 20:02:17 +0530

    Update Valid_Triangle_given_sides.py

--- GeeksForGeeks/GeometryAlgorithms/Triangle/Valid_Triangle_given_sides.py
+++ GeeksForGeeks/GeometryAlgorithms/Triangle/Valid_Triangle_given_sides.py
@@ -1,1 +1,23 @@
 # https://www.geeksforgeeks.org/check-whether-triangle-valid-not-sides-given/
+"""
+
+ A triangle is valid if sum of its two sides is greater than the third side. If three sides are a, b and c, then three conditions should be met. 
+
+(a + b) > c
+(a + c) > b
+(b + c) > a 
+"""
+
+def valid_triangle(a, b, c):
+    if (a + b > c) or (a + c > b) or (b + c > a):
+       return True
+    else:
+       return False
+
+a = 7
+b = 10
+c = 5
+if checkValidity(a, b, c):
+    print("Valid") 
+else:
+    print("Invalid")
