1b4992bbd8de90e697f94c5bf5c567dc34b84a7b
Jaykumaran R <60032500+Jaykumaran@users.noreply.github.com>
2025-05-13 18:16:20 +0530

    Update line_inside_polygon.py

--- GeeksForGeeks/GeometryAlgorithms/polygon/line_inside_polygon.py
+++ GeeksForGeeks/GeometryAlgorithms/polygon/line_inside_polygon.py
@@ -11,11 +11,15 @@ def is_point_in_polygon(x, y, polygon):
         px2, py2 = polygon[i % n]  # Get the next vertex (modulo wraps to first at the end)
 
         # Check if point y is between the y values of the current edge  
-        
         """
         min(y1, y2) < y <= max(y1, y2)
             Horizontal edges are filtered by this <
         """
+        if is_point_on_edge_vertex(x, y, px1, py1, px2, py2):
+            inside = True
+            k += 1
+            return True, k # Point lies on edge or vertex
+            
         if y > min(py1, py2):
             if y <= max(py1, py2):
                 # check if point x is to the left of the edge 
@@ -40,6 +44,17 @@ def is_point_in_polygon(x, y, polygon):
         px1, py1 = px2, py2
 
     return inside, k
+
+
+def is_point_on_edge_vertex(x, y, x1, y1, x2, y2):
+    # Check if the point (x, y) lies on the line segment from (x1, y1) to (x2, y2)
+    dx = x2 - x1
+    dy = y2 - y1
+    if dx * (y - y1) == dy * (x - x1):  # Collinearity
+        if min(x1, x2) <= x <= max(x1, x2) and min(y1, y2) <= y <= max(y1, y2):
+            return True
+    return False
+
 
 
 polygon = [(0, 0), (10, 0), (10, 10), (0, 10)]
